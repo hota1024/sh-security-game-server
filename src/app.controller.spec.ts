2ba8140302206371f477eaf213b7b53817a1f67a
hota1024 <hotalog@hota1024.com>
2021-05-20 21:31:12 +0900

    ✨ add /evaluate (POST)

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -18,5 +18,9 @@ describe('AppController', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!')
     })
+
+    it('should return password level', () => {
+      expect(appController.evaluate({ password: 'Wksrbx/-C.7wHUcV' })).toBe(4)
+    })
   })
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common'
+import * as zxcvbn from 'zxcvbn'
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!'
+  }
+
+  evaluate(password: string) {
+    const result = zxcvbn(password)
+
+    return result.score
   }
 }
